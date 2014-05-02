a7ee051e0e90c04494b422326a7a98caf5013ccf
daserrat <daserrat@htwg-konstanz.de>
2014-05-02 23:37:08 +0200

    Suche - Seite mit javascript

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,6 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -24,4 +23,17 @@ public class Application extends Controller {
     public static Result register(){
     	return ok(ure.render());
     }
+    
+    public static Result impressum(){
+    	return ok(Impressum.render());
+    }
+    
+    public static Result kontakt(){
+    	return ok(Kontakt.render());
+    }
+    
+    public static Result suche(){
+    	return ok(Suche.render());
+    }
+    
 }
