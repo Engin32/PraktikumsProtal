aef729c1c62f2bcb93378c98460bc66dba295bd1
AslanTugrul <tuaslan@htwg-konstanz.de>
2014-05-02 14:10:01 +0200

    Hallo Engin und Daniele mit diesem commit ist die Datenbank eingebunden=) MfG Tugrul Aslan CEO und Vorstandsmitglied TED GmbH

--- app/DB/DBConnector.java
+++ app/DB/DBConnector.java
@@ -9,17 +9,18 @@ public class DBConnector {
 	Connection con;
 
 	public DBConnector() {
-		
-		try{
-			
-			Class.forName("com.mysql.jdbc.Driver");
-			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Praktikumsportal","root","");
-			 System.out.println("alles in Ordnung");
 
-			
-		}catch(Exception ex){
-			System.out.println("Dieser Fehler ist aufgetreten: " + ex.getMessage());
-			
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/Praktikumsportal", "root", "");
+			System.out.println("alles in Ordnung");
+
+		} catch (Exception ex) {
+			System.out.println("Dieser Fehler ist aufgetreten: "
+					+ ex.getMessage());
+
 		}
 
 	}
