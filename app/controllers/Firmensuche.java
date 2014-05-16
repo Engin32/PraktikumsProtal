59790c63b93b56baed3cd51d2a17eeeb2e5578c5
Engin32 <enaslan@htwg-konstanz.de>
2014-05-16 13:55:28 +0200

    Ajax entgültig mit db

--- app/controllers/Firmensuche.java
+++ app/controllers/Firmensuche.java
@@ -12,82 +12,60 @@ import views.html.*;
 
 import java.sql.*;
 
-
 public class Firmensuche extends Controller {
 
+	public static Result suche(String daten) throws SQLException {
 
-	
-	public static Result suche(String daten) throws SQLException{
-		
 		ResultSet rs;
 		Connection con;
-		String ergebnis="";
+		String ergebnis = "";
 
-		
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/Praktikumsportal", "root", "");
 			System.out.println("alles in Ordnung");
-			
-			
+
 			Statement stmt = con.createStatement();
 			rs = stmt.executeQuery("select untname from Unternehmen");
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				String unternehmen = rs.getString("untname");
-				
-					if(daten.equals(unternehmen)){
-						ergebnis=unternehmen;
-						
-					}else{
-						ergebnis="Kein Ergebnis";
-					}
-				
-				
+
+				if (daten.equals(unternehmen)) {
+					ergebnis = unternehmen +" ist bei uns registriert!";
+					break;
+				} else {
+					ergebnis = "Kein Ergebnis";
+				}
+
 			}
-			
-			
-			
-			
-			
-			
 
 		} catch (Exception ex) {
 			System.out.println("Dieser Fehler ist aufgetreten: "
 					+ ex.getMessage());
 
 		}
-		
-	
-		
-		
-		
-		
+
 		System.out.println("Hier werden die firmen abgerufen");
-		String[] unternehmen = {"Daimler","BMW","Bosch" };
+		/*
+		String[] unternehmen = { "Daimler", "BMW", "Bosch" };
 		System.out.println(daten);
-		
-		
-		for(int i =0; i<unternehmen.length; i++){
-			
-			if(daten.equals(unternehmen[i])){
-				ergebnis=unternehmen[i];
+
+		for (int i = 0; i < unternehmen.length; i++) {
+
+			if (daten.equals(unternehmen[i])) {
+				ergebnis = unternehmen[i] +" ist bei uns regisrtiert!";
 				break;
-			}else{
-				ergebnis="kein Ergebnis";
+			} else {
+				ergebnis = "kein Ergebnis";
 			}
-			
-		}
-		
-		
-		return ok("Hier ist das gesuchte Ergbenis: " +ergebnis);
-		
-		
+
+		}*/
+
+		return ok("Hier ist das gesuchte Ergbenis: " + ergebnis);
+
 	}
-	
-	
-	
 
 }
--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,55 +1,81 @@
 package controllers;
 
 import java.security.Policy.Parameters;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Map;
 
 import play.*;
 import play.mvc.*;
 import play.mvc.Http.RequestBody;
 import views.html.*;
+import java.sql.*;
 
 public class Login extends Controller {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 	public static Result anmeldenUnternehmen() {
+		
+		
 		// Daten rausholen und in Map reinspeichern
-		Map<String, String[]> daten = request().body().asFormUrlEncoded();
-		// Daetn rausnehemen von der Map
-		String email = daten.get("email")[0];
-		String password = daten.get("password")[0];
+				Map<String, String[]> daten = request().body().asFormUrlEncoded();
+				// Daetn rausnehemen von der Map
+				String email = daten.get("email")[0];
+				String password = daten.get("password")[0];
 		
 		
-		
-		
-		if (daten == null) {
-			
-			return null;
+		ResultSet rs;
+		Connection con;
+		boolean regisrtiert = false;
 
-		} else {
-		
-		System.out.println("hallo das ist deine Email: "+email +"und das dein Password: "+password);
-			return ok(afterloginUnternehmen.render());
-		
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/Praktikumsportal", "root", "");
+			System.out.println("alles in Ordnung");
+
+			Statement stmt = con.createStatement();
+			rs = stmt.executeQuery("select untID, passwort from Unternehmen");
+
+			while (rs.next()) {
+				String unternehmen = rs.getString("untID");
+				String passwort = rs.getString("passwort");
+				if(email.equals(unternehmen) && password.equals(passwort)){
+					
+					regisrtiert=true;
+				}
+				
+
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Dieser Fehler ist aufgetreten: "
+					+ ex.getMessage());
+
 		}
-	}
-	
-	
-	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
-	
-	
-	
-	
+
+			System.out.println("hallo das ist deine Email: " + email
+					+ "und das dein Password: " + password);
+			if(regisrtiert==true){
+			return ok(afterloginUnternehmen.render());
+			}else{
+				return ok("Email oder Passwort falsch!");
+			}
+		
+	}
 
 }
