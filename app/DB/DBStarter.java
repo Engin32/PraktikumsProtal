42cfb223e4b6a6fbe7b7fc7c6a0c1f3b9cff7e1d
AslanTugrul <tuaslan@htwg-konstanz.de>
2014-05-30 14:09:38 +0200

    DB-Tester

--- app/DB/DBStarter.java
+++ app/DB/DBStarter.java
@@ -1,6 +1,9 @@
 package DB;
 
 public class DBStarter {
+	
+	// Diese Klasse dient nur zur einfachen überprüfung der DB Verbindung
+
 	public static void main(String[] args) {
 		DBConnector connection = new DBConnector();
 	}
